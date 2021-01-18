df33fc6426dcc5482e015ef8ba8035e577f75926
鈴木泰斗 <suzukitaito@suzukitaitos-MacBook-Pro.local>
2021-01-18 22:04:16 +0900

    save

--- entity/model/message.go
+++ entity/model/message.go
@@ -1,8 +1,13 @@
 package model
 
 import (
+	"mime"
+	"net/http"
 	"regexp"
+	"strconv"
 
+	"github.com/otiai10/opengraph/v2"
+	"golang.org/x/xerrors"
 	"mvdan.cc/xurls/v2"
 )
 
@@ -33,8 +38,11 @@ type MessageEntity struct {
 // MessageEntityURL ...
 type MessageEntityURL struct {
 	MessageEntity
-	URL        string
-	DisplayURL string
+	MediaType     string // RFC-1521
+	ContentLength int64
+	URL           string
+	DisplayURL    string
+	OpenGraph     *OpenGraph
 }
 
 // MessageEntityMention ...
@@ -62,8 +70,11 @@ func ParseURL(text string, me *MessageEntities) {
 					End:   index[1],
 				},
 			},
-			URL:        uri,
-			DisplayURL: uri,
+			URL:           uri,
+			DisplayURL:    uri,
+			MediaType:     "",
+			OpenGraph:     nil,
+			ContentLength: 0,
 		}
 		me.URLs = append(me.URLs, m)
 	}
@@ -86,4 +97,175 @@ func ParseMention(text string, me *MessageEntities) {
 		}
 		me.Mentions = append(me.Mentions, m)
 	}
+}
+
+// OpenGraph ...
+type OpenGraph struct {
+	Title       string
+	Type        string
+	Image       []OpenGraphImage
+	URL         string
+	Audio       []OpenGraphAudio
+	Description string
+	Determiner  string
+	Locale      string
+	LocaleAlt   []string
+	SiteName    string
+	Video       []OpenGraphVideo
+}
+
+// OpenGraphImage ...
+type OpenGraphImage struct {
+	URL       string
+	SecureURL string
+	Type      string
+	Width     int
+	Height    int
+	Alt       string
+}
+
+// OpenGraphAudio ....
+type OpenGraphAudio struct {
+	URL       string
+	SecureURL string
+	Type      string
+}
+
+// OpenGraphVideo ...
+type OpenGraphVideo struct {
+	URL       string
+	SecureURL string
+	Type      string
+	Width     int
+	Height    int
+	Duration  int
+}
+
+func newOpenGraphFromOpenGraphGo(ogp *opengraph.OpenGraph) *OpenGraph {
+	return &OpenGraph{
+		Title:       ogp.Title,
+		Type:        ogp.Type,
+		Image:       newOpenGraphImageFromOpenGraphGo(ogp.Image),
+		URL:         ogp.URL,
+		Audio:       newOpenGraphAudioFromOpenGraphGo(ogp.Audio),
+		Description: ogp.Description,
+		Locale:      ogp.Locale,
+		LocaleAlt:   ogp.LocaleAlt,
+		SiteName:    ogp.SiteName,
+		Video:       newOpenGraphVideoFromOpenGraphGo(ogp.Video),
+	}
+}
+
+func newOpenGraphImageFromOpenGraphGo(image []opengraph.Image) []OpenGraphImage {
+	r := []OpenGraphImage{}
+	for _, i := range image {
+		r = append(r, OpenGraphImage{
+			URL:       i.URL,
+			SecureURL: i.SecureURL,
+			Type:      i.Type,
+			Width:     i.Width,
+			Height:    i.Height,
+			Alt:       i.Alt,
+		})
+	}
+	return r
+}
+
+func newOpenGraphAudioFromOpenGraphGo(audio []opengraph.Audio) []OpenGraphAudio {
+	r := []OpenGraphAudio{}
+	for _, i := range audio {
+		r = append(r, OpenGraphAudio{
+			URL:       i.URL,
+			SecureURL: i.SecureURL,
+			Type:      i.Type,
+		})
+	}
+	return r
+}
+
+func newOpenGraphVideoFromOpenGraphGo(video []opengraph.Video) []OpenGraphVideo {
+	r := []OpenGraphVideo{}
+	for _, i := range video {
+		r = append(r, OpenGraphVideo{
+			URL:       i.URL,
+			SecureURL: i.SecureURL,
+			Type:      i.Type,
+			Width:     i.Width,
+			Height:    i.Height,
+			Duration:  i.Duration,
+		})
+	}
+	return r
+}
+
+func fetchOpenGraph(url string) (*OpenGraph, error) {
+	ogp, err := opengraph.Fetch(url)
+	if err != nil {
+		return nil, xerrors.Errorf("Cannot fetch ogp : %w", err)
+	}
+	return newOpenGraphFromOpenGraphGo(ogp), nil
+}
+
+func HeadURL(cli *http.Client, me *MessageEntities) []error {
+	errs := []error{}
+	for i := range me.URLs {
+		res, err := cli.Head(me.URLs[i].URL)
+		if err != nil {
+			errs = append(errs, err)
+			continue
+		}
+
+		contentType := ""
+		contentType = res.Header.Get("Content-Type")
+		if contentType == "" {
+			contentType = res.Header.Get("content-type")
+		}
+		if contentType == "" {
+			contentType = res.Header.Get("Content-type")
+		}
+		if contentType == "" {
+			contentType = res.Header.Get("content-Type")
+		}
+		mediaType, _, err := mime.ParseMediaType(contentType)
+		if err != nil {
+			errs = append(errs, err)
+		} else {
+			me.URLs[i].MediaType = mediaType
+		}
+
+		contentLength := ""
+		contentLength = res.Header.Get("Content-Length")
+		if contentLength == "" {
+			contentLength = res.Header.Get("content-length")
+		}
+		if contentLength == "" {
+			contentLength = res.Header.Get("Content-length")
+		}
+		if contentLength == "" {
+			contentLength = res.Header.Get("content-Length")
+		}
+		contentLengthInt, err := strconv.ParseInt(contentLength, 10, 64)
+		if err != nil {
+			errs = append(errs, err)
+		} else {
+			me.URLs[i].ContentLength = contentLengthInt
+		}
+	}
+	return errs
+}
+
+func FetchOpenGraph(cli *http.Client, me *MessageEntities) []error {
+	errs := []error{}
+	for i := range me.URLs {
+		if me.URLs[i].MediaType != "text/html" {
+			continue
+		}
+		ogp, err := fetchOpenGraph(me.URLs[i].URL)
+		if err != nil {
+			errs = append(errs, err)
+			continue
+		}
+		me.URLs[i].OpenGraph = ogp
+	}
+	return errs
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module github.com/suzuito/message-go
 go 1.15
 
 require (
+	github.com/otiai10/opengraph v1.1.3 // indirect
+	github.com/otiai10/opengraph/v2 v2.0.2
 	github.com/stretchr/testify v1.6.1
+	github.com/tdewolff/parse/v2 v2.5.7 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
 	mvdan.cc/xurls v1.1.0
 	mvdan.cc/xurls/v2 v2.2.0
 )
